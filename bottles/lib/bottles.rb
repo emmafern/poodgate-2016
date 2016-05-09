22d675ce051053595afc42072ad0cd1f8da22132
Emma Colner <emma@stitchfix.com>
2016-05-09 11:14:01 -0700

    bottles solution kwu emma jan

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,3 +1,45 @@
 class Bottles
 
+  def verse(num_bottles)
+    next_num = (num_bottles - 1) % 100
+    "#{generate_number_string(num_bottles).capitalize} of beer on the wall, " +
+    "#{generate_number_string(num_bottles)} of beer.\n" +
+    next_action(num_bottles) +
+    "#{generate_number_string(next_num)} of beer on the wall.\n"
+  end
+
+  def verses(num_bottles, next_num)
+    verse(num_bottles) +
+    "\n" +
+    verse(next_num)
+  end
+
+  def pluralize(num)
+    if num == 1
+      'bottle'
+    elsif num > 1
+      'bottles'
+    end
+  end
+
+  def generate_number_string(num)
+    if num > 0
+      "#{num} #{pluralize(num)}"
+    else
+      'no more bottles'
+    end
+  end
+
+  def one_or_it(num)
+    num == 1 ? 'it' : 'one'
+  end
+
+  def next_action(num)
+    if num == 0
+      "Go to the store and buy some more, "
+    else
+      "Take #{one_or_it(num)} down and pass it around, "
+    end
+  end
+
 end
